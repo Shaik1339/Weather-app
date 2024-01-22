5d0e76f902a773c60ce43750ff41ae5ef0aacb18
Shaik1339 <musthashaik@gmail.com>
2024-01-22 21:03:13 +0530

    second commit

--- app.js
+++ app.js
@@ -10,18 +10,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req,res) => {
 
-    res.send("this for weather checing")
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 
 })
 
 app.post('/',(req,res)=>{
 
-console.log(req.body.city);
+
 const city=req.body.city;
   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=068134c7e140a834461011d7014df04d&units=metric`)
   .then(response => {
-    console.log(response.data.main)
     
     res.send(`<h2>The temparature in ${city}  is ${response.data.main.temp} celsius degress</h2>`);
   })
